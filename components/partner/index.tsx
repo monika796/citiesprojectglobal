da8c5f9b72d1eb81a3982113981d211e2cf95a30
unknown <prabhatmohal1998@gmail.com>
2025-01-06 21:54:22 +0530

    partner changes

--- components/partner/index.tsx
+++ components/partner/index.tsx
@@ -101,17 +101,17 @@ console.log(partner_immm);
         <div className="mx-auto  px-4 md:px-8 2xl:px-0">
           <h5 className="md:text-[64px] text-[30px] text-black text-center md:pb-[30px] font-bold pb-2">{data.page.patnerssection.mainheading}</h5>
           <h5 className="text-[15px] text-black text-center">{data.page.patnerssection.firstsubtitlepatner}</h5>
-          <div className="  grid grid-cols-2 items-center sm:flex sm:flex-wrap justify-center gap-2 md:w-[100%] lg:w-[100%] mx-auto pt-[29px]">
+          <div className="  grid md:grid-cols-5 grid-cols-2  items-center sm:flex sm:flex-wrap justify-center gap-5 md:w-[100%] lg:w-[100%] mx-auto pt-[29px]">
           {partner_immm.map((partner_images, index) => (
-          <div className=""> <Image src={partner_images.addPartnersImages?.node?.link || '/No_Image.jpg'} alt='' width={150} height={150} className=" p-[33px] border border-[#e3e2e2] object-contain " /> 
+          <div className=" p-[33px] border border-[#e3e2e2] h-[250px] grid items-center grayscale hover:grayscale-0"> <Image src={partner_images.addPartnersImages?.node?.link || '/No_Image.jpg'} alt='' width={150} height={150} className="  object-contain " /> 
             </div>
         ))}
    
               
-            <div className=" relative bg-black text-white p-[27px] w-[150px] h-[150px] flex items-center justify-center md:mx-0 lg:mx-0">
-              <div className="text-left absolute bottom-[15px] w-[89%]">
-                <h5 className="text-[12px] font-bold pb-2">{data.page.patnerssection.textninthbox}</h5>
-                <Link href={data.page.patnerssection.linknithbox}><h3 className="text-[10px] font-bold">{data.page.patnerssection.linktextninthbox}</h3></Link>
+            <div className=" relative bg-black text-white p-[20px] md:w-[223.29px] h-[250px] flex items-end justify-center md:mx-0 lg:mx-0">
+              <div className="text-left  w-[89%]">
+                <h5 className="text-[20px] w-[122px] font-bold pb-2">{data.page.patnerssection.textninthbox}</h5>
+                <Link href={data.page.patnerssection.linknithbox}><h3 className="text-[16px] font-normal underline">{data.page.patnerssection.linktextninthbox}</h3></Link>
               </div>
             </div>
 
